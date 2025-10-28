ecea4e97e961ce91e1f0ce8c6225f61f6e178665
WalkerJei <jylee34@outlook.com>
2025-10-28 19:43:19 +0900

    Update 20251028 Minor_1 / 0.11.0
    Update Unity Engine version to 6000.0.59f2
    Remove many files causing errors
    Reset the project as initial and begin redevelopment

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+
+    // 생명력과 최대 생명력 
+    [SerializeField] ushort Hp;
+    [SerializeField] ushort maxHp;
+
+    //
+    [SerializeField] float walkSpeed;
+    public float WalkSpeed
+    {
+        get { return walkSpeed; }
+        set { walkSpeed = value; }
+    }
+
+    [SerializeField] float runSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : Entity
+{
+    // 경험치와 최대 경험치
+    [SerializeField] int exp;
+    [SerializeField] int maxExp;
+
+    // 스태미나와 최대 스태미나
+    [SerializeField] ushort stamina;
+    [SerializeField] ushort maxStamina;
+
+    // 포만감과 최대 포만감
+    [SerializeField] ushort satiety;
+    [SerializeField] ushort maxSatiety;
+
+    // 수분과 최대 수분
+    [SerializeField] ushort hydration;
+    [SerializeField] ushort maxHydration;
+
+    // 체온
+    [SerializeField] float bodyTemperature;
+
+    Vector2 moveDirection;
+
+    new Rigidbody2D rigidbody2D;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void OnMove(InputAction.CallbackContext inputAction)
+    {
+        moveDirection = inputAction.ReadValue<Vector2>();
+    }
+
+    public void Move()
+    {
+        rigidbody2D.linearVelocity = moveDirection * WalkSpeed;
+    }
+}
